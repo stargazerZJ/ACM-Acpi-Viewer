e48bb62863caedcddebfda2ddb6f453f6d00cc2d
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2025-05-06 22:22:08 +0800

    feat: uefi runtime service added via uefi variables

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,28 @@
 #![no_main]
 #![no_std]
+#![feature(naked_functions)]
 
-use uefi::{guid, prelude::*, println, Error, Guid};
-use uefi::boot::install_protocol_interface;
-use uefi::proto::unsafe_protocol;
+use core::arch::naked_asm;
+use uefi::runtime::{VariableVendor, set_variable, VariableAttributes};
+use uefi::{Guid, guid, prelude::*, println};
 
-const PROTO_GUID: Guid = guid!("9f01e43e-b2a1-4774-9ea8-a51ffd6d30fc");
-
-#[derive(Debug)]
-#[repr(C)]
-pub struct MyProtocol {
-    pub test_fn: unsafe extern "efiapi" fn(this: *mut Self, input: i32, output: *mut i32) -> Status
-}
-
-impl MyProtocol {
-    /// test_fn takes an integer i and returns i * i
-    pub unsafe extern "efiapi" fn test_fn(_this: *mut Self, input: i32, output: *mut i32) -> Status {
-        if output.is_null() {
-            return Status::INVALID_PARAMETER;
-        }
-        unsafe {
-            *output = input * input;
-        }
-        Status::SUCCESS
-    }
-}
-
-#[unsafe_protocol(PROTO_GUID)]
-pub struct MyProtocolInterface(MyProtocol);
-
-impl MyProtocolInterface {
-    pub fn test_fn(&mut self, input: i32) -> Result<i32, Error> {
-        let mut output: i32 = 0;
-        let status = unsafe { (self.0.test_fn)(&mut self.0, input, &mut output) };
-        status.to_result_with_val(|| output)
-    }
-    
+const SERVICE_GUID: Guid = guid!("9f01e43e-b2a1-4774-9ea8-a51ffd6d30fc");
+/// test_fn takes an integer i and returns i * i
+#[naked]
+pub unsafe extern "efiapi" fn test_fn() {
+    naked_asm!(
+        "mov rax, rdi",     // rdi contains the u64 input argument
+        "mul rax",          // multiply rax by rax, result stored in rdx:rax (high:low)
+        "ret",              // return value in rax
+    );
+    // (input * input) as usize
+    // if output.is_null() {
+    //     return Status::INVALID_PARAMETER;
+    // }
+    // unsafe {
+    //     *output = input * input;
+    // }
+    // Status::SUCCESS
 }
 
 #[entry]
@@ -44,31 +31,22 @@ fn main() -> Status {
 
     println!("Hello from UEFI!");
 
-    let my_protocol = MyProtocol {
-        test_fn: MyProtocol::test_fn,
-    };
+    let service_name = cstr16!("MyRuntimeService");
 
-    let result = unsafe {
-        install_protocol_interface(
-            None,
-            &PROTO_GUID,
-            &my_protocol as *const _ as *const _
-        )
-    };
+    let test_fn_bytes = (test_fn as usize).to_ne_bytes();
 
-    let handle = result.unwrap();
+    println!("Address of test_fn: {:#x}", test_fn as usize);
 
-    println!("Protocol installed with handle: {:?}", handle);   // seems to always be Handle(0x7e1b1f98)
+    let result = set_variable(
+        service_name,
+        &VariableVendor(SERVICE_GUID),
+        VariableAttributes::BOOTSERVICE_ACCESS | VariableAttributes::RUNTIME_ACCESS,
+        &test_fn_bytes,
+    );
 
-    let mut opened_protocol = boot::open_protocol_exclusive::<MyProtocolInterface>(handle).unwrap();
-
-    let input = 5;
-    let mut output: i32 = 0;
-    let status = unsafe { (opened_protocol.0.test_fn)(&mut opened_protocol.0, input, &mut output) };
-
-    match status {
-        Status::SUCCESS => println!("test_fn({}): {}", input, output),
-        _ => println!("Failed to call test_fn: {:?}", status),
+    match result {
+        Ok(_) => println!("Successfully set variable"),
+        Err(e) => println!("Failed to set variable: {:?}", e),
     }
 
     Status::SUCCESS
